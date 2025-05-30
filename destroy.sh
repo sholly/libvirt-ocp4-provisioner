f6a5a5c93bd161f055e4997e6aa437fc164001c8
jay <j@zuul.org>
2025-05-29 21:34:06 -0400

    multiple clusters works

--- destroy.sh
+++ destroy.sh
@@ -1,11 +1,11 @@
 for f in bastion loadbalancer bootstrap master-0 master-1 master-2 worker-0 worker-1 
 do
-	v=multidev-$f
+	v=ocp4-$f
 	echo $v
 	virsh destroy $v
 	virsh undefine --remove-all-storage $v
 done
-virsh net-destroy multidev
-virsh net-undefine multidev
-virsh pool-destroy multidev
-virsh pool-undefine multidev
+virsh net-destroy ocp4
+virsh net-undefine ocp4
+virsh pool-destroy ocp4
+virsh pool-undefine ocp4
